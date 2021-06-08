44ce61869992796f75142b0a6027543b459977ea
alanmontgomery <alan.montgomery@tascomi.com>
2021-06-08 23:45:28 +0100

    Initial commit

---
+++ src/components/AddToCartButton.js
@@ -0,0 +1,63 @@
+import { CreateAnimation, IonButton, IonIcon } from '@ionic/react';
+import { useRef, useState } from 'react';
+
+import styles from "./AddToCartButton.module.css";
+import { cartOutline } from "ionicons/icons";
+
+const AddToCartButton = ({ icon = true, color = "primary", customOnClick = null }) => {
+
+	const iconRef = useRef();
+  	const [ hidden, setHidden ] = useState(true);
+
+	const floatStyle = {
+
+		display: hidden ? "none" : "",
+		marginLeft: icon ? "-3.7rem" : "-5.2rem",
+		position: "absolute"
+	};
+
+	const floatGrowAnimation = {
+
+		property: "transform",
+		fromValue: "translateY(0) scale(1)",
+		toValue: "translateY(-40px) scale(1.2)"
+    };
+
+	const colorAnimation = {
+    	
+		property: "color",
+      	fromValue: "white",
+      	toValue: `var(--ion-color-${ color }`
+    };
+
+	const mainAnimation = {
+		
+		duration: 600,
+		iterations: "1",
+		fromTo: [ floatGrowAnimation, colorAnimation ],
+		easing: "cubic-bezier(0.25, 0.7, 0.25, 0.7)"
+	};
+
+	const handleClick = async () => {
+
+		setHidden(false);
+		await iconRef.current.animation.play();
+		setHidden(true);
+		customOnClick && customOnClick();
+	}
+
+	return (
+		<div className={ styles.buttonContainer }>
+			<IonButton color={ color } className={ styles.button } onClick={ handleClick }>
+          		{ !icon && "Add to cart" }
+				{ icon && <IonIcon icon={ cartOutline } /> }
+        	</IonButton>
+
+        	<CreateAnimation ref={ iconRef } { ...mainAnimation }>
+          		<IonIcon icon={ cartOutline } size="large" style={ floatStyle } />
+        	</CreateAnimation>
+		</div>
+	);
+}
+
+export default AddToCartButton;
